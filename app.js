f399cf49556890c8c1a5fee8b6c00e63a99ab854
Müller Csaba <csaba.muller@hotmail.com>
2023-05-23 21:50:04 +0200

    finalize

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const leftPage = document.querySelector(".left");
-const leftPageBefore = document.querySelector(".split-left-side::before");
 const rightPage = document.querySelector(".right");
 const container = document.querySelector(".container");
 
+leftPage.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
 
-leftPage.addEventListener("mouseenter", ()=>{
-    container.classList.add("hover-left");  
-})
+leftPage.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
+});
 
-leftPage.addEventListener("mouseleave", ()=>{
-     container.classList.remove("hover-left");
-})
+rightPage.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
 
-
-rightPage.addEventListener("mouseenter", ()=>{
-    container.classList.add("hover-right");  
-})
-
-rightPage.addEventListener("mouseleave", ()=>{
-     container.classList.remove("hover-right");
-})
+rightPage.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
